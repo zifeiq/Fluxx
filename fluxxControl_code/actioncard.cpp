1c16a2ef45c52c5242df3a520120ee06679ed24c
Ivan-wang <ivan.wang.sh@gmail.com>
2014-06-03 20:46:54 +0800

    声明了当前行动卡的函数名

---
+++ fluxxControl_code/actioncard.cpp
@@ -0,0 +1,60 @@
+void draw_2_and_play_2() {
+	//抓2用2
+}
+void drop_and_draw() {
+	//弃牌重抓
+}
+void reset_all_rules() {
+	//重置规则
+}
+void draw_3_and_play_2() {
+	//抓3用2
+}
+void win_the_lottery() {
+	//彩票中奖
+}
+void lets_do_it_again() {
+	//再来一次
+}
+void lets_simplify() {
+	//简化规则
+}
+void exchange_keeper() {
+	//交换所有物
+}
+void another_round() {
+	//再动一轮
+}
+void steal_a_keeper() {
+	//偷窃所有物
+}
+void random_tax() {
+	//抽税
+}
+void remove_a_rule() {
+	//取消一条规则
+}
+void exchange_hand() {
+	//交换手牌
+}
+void everyone_gets_1() {
+	//每人一张
+}
+void take_1_and_use_it() {
+	//取而用之
+}
+void clean_the_trash() {
+	//清除垃圾
+}
+void remove_limitation() {
+	//取消上限
+}
+void redistribute_keeper() {
+	//轮转周牌
+}
+void remove_a_keeper() {
+	//丢弃
+}
+void rotate_hand() {
+	//轮转手牌
+}
