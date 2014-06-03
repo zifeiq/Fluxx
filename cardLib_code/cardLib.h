f5e469659e8da9bd662728dd5d0e6ed2761ea492
liucongliuyi <liucongliuyi@163.com>
2014-06-03 16:13:20 +0800

    增加了include文件流头文件

--- cardLib_code/cardLib.h
+++ cardLib_code/cardLib.h
@@ -1,58 +1,58 @@
 #include <vector>
 #include <fstream>
 
-class Card  //���ƵĻ���
+class Card  //卡牌的基类
 {
 public:
-	Card() :_type(BASIC_RULE), _num(0){} //Ĭ�Ϲ��캯��
+	Card() :_type(BASIC_RULE), _num(0){} //默认构造函数
 	enum Type{BASIC_RULE, NEW_RULE, KEEPER, GOAL, ACTION};
-	Type getType() const{ return _type; } //���ر���������
-	int getNum() const{ return _num; }  //���ر����ı���
+	Type getType() const{ return _type; } //返回本卡的属性
+	int getNum() const{ return _num; }  //返回本卡的编号
 	virtual ~Card(){}
 	bool operator==(const Card& c) const;
 protected:
 	Type _type;
-	int _num; //�����Ƶı��ţ���λ��
+	int _num; //本卡牌的编号（两位）
 };
 
-class RuleCard :public Card  //������
+class RuleCard :public Card  //规则牌
 {
 public:
 	RuleCard() :Card(){}
 	RuleCard(int num){ _type = NEW_RULE; _num = num; }
 };
 
-class BasicRuleCard : public RuleCard //����������
+class BasicRuleCard : public RuleCard //基本规则牌
 {
 public:
 	BasicRuleCard() :RuleCard(){}
 };
 
-class NewRuleCard :public RuleCard  //�¹�����
+class NewRuleCard :public RuleCard  //新规则牌
 {
 public:
 	NewRuleCard(int num) :RuleCard(num){}
 };
 
-class KeeperCard :public Card  //�����￨��
+class KeeperCard :public Card  //所有物卡牌
 {
 public:
 	KeeperCard(int num) { _type = KEEPER; _num = num; }
 };
 
-class GoalCard :public Card  //Ŀ����
+class GoalCard :public Card  //目标牌
 {
 public:
 	GoalCard(int num) { _type = GOAL; _num = num; }
 };
 
-class ActionCard :public Card  //�ж���
+class ActionCard :public Card  //行动牌
 {
 public:
 	ActionCard(int num){ _type = ACTION; _num = num; }
 };
 
-class CardLib  //�ƿ⣬singletonģʽ
+class CardLib  //牌库，singleton模式
 {
 public:
 	static CardLib& getLib()
@@ -66,6 +66,6 @@ public:
 protected:
 	CardLib();
 	~CardLib();
-	std::vector<Card*> _cards;  //������Ϊ63��, �ж������û���չ��(How????)  ���ڿɼ�
+	std::vector<Card*> _cards;  //基本卡为63个, 行动卡和用户拓展卡(How????)  后期可加
 	int _cardNum;
 };
