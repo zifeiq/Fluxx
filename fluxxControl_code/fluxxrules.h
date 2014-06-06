ab5d3994e515f0e5a6f811882f06a5673b3829d5
Ivan-wang <ivan.wang.sh@gmail.com>
2014-06-06 23:14:32 +0800

    这次rule改的有点多，把每一个特殊规则都列了出来，原来CPP文件中的大部分函数都没用了，先传个头文件，方便大家调用，CPP改好之后再传。

--- fluxxControl_code/fluxxrules.h
+++ fluxxControl_code/fluxxrules.h
@@ -3,38 +3,43 @@
 
 #include <vector>
 #include "..\cardLib_code\cardLib.h"
-#define CLOCKWISE false
-#define ANTTCLOCKWISE true
+//#define CLOCKWISE false
+//#define ANTTCLOCKWISE true
 
-enum specialRules {
-	NONE = 0, RICH, POOR, INFLATION, ROTATE, NOHAND, RANDOM, DOUBLEGOALS
-};
 class fluxxRules {
 public:
 	fluxxRules ();
-
-	void draw(int);
-	void play(int);
-	void cardlimitation(int);
-	void keeperlimitation(int);
-	void direction(bool);
-	void firstgoal(const GoalCard&);
-	void secondgoal(const GoalCard&);
-	void setspecialrule();
-	void addrule(const RuleCard&);
-	void removerule(const RuleCard&);
-	void getspecialrule();
+	//设定参数
+	void setdraw(int i) { cntDraw = i; }
+	void setplay(int i) { cntPlay = i; }
+	void setcardlimitation(int i) { cntCardlimit = i; }
+	void setkeeperlimitation(int i) { cntKeeperlimit = i; }
+	void setorderreverse(bool b) { orderReverse = b; }
+	void setrichbonus(bool b) { richBonus = b; }
+	void setpoorbonus(bool b) { poorBonus = b; }
+	void setinflation(bool b) { Inflation = b; }
+	void setnohandbonus(bool b) { noHandbonus = b; }
+	void setrandomstart(bool b) { randomStart = b; }
+	//设定有关卡牌
 	void setdrawrule(const Card&);
 	void setplayrule(const Card&);
 	void sethandlimrule(const Card&);
 	void setkeeperlimrule(const Card&);
-
-	const int draw()const;
-	const int play () const;
-	const int cardlimitation() const;
-	const int keeperlimitation() const;
-	const bool direction() const;
+	void setfirstgoal(const GoalCard&);
+	void setsecondgoal(const GoalCard&);
+	//获取参数
+	const int getdraw()const;
+	const int getplay () const;
+	const int getcardlimitation() const;
+	const int getkeeperlimitation() const;
+	const bool isorderreverse() const{ return orderReverse; }
 	const bool isdoublegoals() const {return doubleGoles;}
+	const bool isrichBonus() const { return richBonus; }
+	const bool ispoorBonus() const { return poorBonus; }
+	const bool isinflation() const { return Inflation; }
+	const bool isnohandbonus() const { return noHandbonus; }
+	const bool israndomstart() const { return randomStart; }
+	//获取卡牌
 	const Card& firstgoal() const;
 	const Card& secondgoal() const;
 	const std::vector<RuleCard&>& getrulecard();
@@ -42,6 +47,7 @@ public:
 	~fluxxRules ();
 
 private:
+	//规则的标志位和对应的卡牌
 	int cntDraw;//摸牌数
 	Card& DrawRule;
 	int cntPlay;//出牌数
@@ -49,13 +55,22 @@ private:
 	int cntCardlimit;//手牌上限
 	Card& CardlimitRule;
 	int cntKeeperlimit;//所有物上限
-	Card& Cardlimiton
-	bool Direction;//出牌方向
-	bool doubleGoles;
+	Card& Cardlimiton;
+	bool orderReverse;//出牌方向
+	Card& c_orderReverse;
+	bool doubleGoles;//双重目标
 	Card& firstGoal;
 	Card& secondGoal;
-	//std::vector<RuleCard&> rulecards;//给gui提供方便
-	std::vector<specialRules> curRules; 
+	bool richBonus;//富人奖励
+	Card& c_richbonus;
+	bool poorBonus;//穷人奖励
+	Card& c_poorBonus;
+	bool Inflation;//通货膨胀
+	Card& c_Inflation;
+	bool noHandbonus;//无手牌奖励
+	Card& c_noHandbonus;
+	bool randomStart;//随机开场
+	Card& c_noRandomstart;
 };
 
 #endif
