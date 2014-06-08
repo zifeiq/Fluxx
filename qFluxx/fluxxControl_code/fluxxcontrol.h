e09c61fa91d07bfe4f73c5afdd1752fc3e76e69b
Ivan-wang <ivan.wang.sh@gmail.com>
2014-06-08 13:05:28 +0800

    ```

--- qFluxx/fluxxControl_code/fluxxcontrol.h
+++ qFluxx/fluxxControl_code/fluxxcontrol.h
@@ -9,50 +9,55 @@
 #include "..\mailbox\serverMB\serverMB.h"
 #include "..\fluxxControl_code\player.h"
 
-typedef int ruleMsg;
 extern enum MsgType;
 enum gameState{ 
 	WAIT_FOR_PLAYERS,
 	DEAL_ORIGINAL_CARD,
+	ROUND_PREPARE,
+	GAME_RUNNING,
+	WAIT_FOR_ACK
 };
 class fluxxControl {
 public:
 	fluxxControl ();
-	//阶段处理函数
-	void initrules();//初始化规则
 	void addPlayers();//添加玩家
-	void shuffleCard();//洗牌
-	void dealCard(); //发牌
-	void playCard();//出牌
-	void dropCard();//弃牌
-	void setpresentPlayer(Player&);//回合控制
-	void stagecontrol();
-	int getclientnum(Player&);
-	void run();
+	/***基本操作函数***/
+	void dealCard(int); //发牌
+	void playCard(const Card&);//出牌
+	void dropCard(int);//弃牌
+	/***************************************/
+	void setpresentPlayer(int);//修改当前前面函数的作用对象
+	//整局游戏控制
+	void fluxxRun();
+
 	~fluxxControl (){};
 
 private:
-	void _settleCard(const Card&); //单张出牌与结算，入口参数为卡牌的引用
-	void _removeCard(Player&);//移除手牌
+	void _shuffleCard();//洗牌
+	void _settleCard(const Card&); //单张结算
 	void _updateRules();
 	void _updateKeepers();
-	int _checkWinner();
+	int _checkCntdraw();//计算摸牌量
+	int _checkCntplay();//计算出牌量
+	int _checkCntdrop();
+	int _checkWinner();//检查胜利
+	int _getclientnum(Player&);//计算用于消息传递的玩家编号
 	//单个回合控制相关
-	Player presentPlayer;
+	Player& presentPlayer;
 	fluxxRules rule;
 	//信息传递相关
 	int msgbufAdditional;//附加消息编号
 	int clientNum;//选中的client编号
 	MsgType msgbufMsgtype;//消息类型缓存
 	std::string msgbufString;//字符串缓存
-	std::vector<Card*> msgbufCards;//卡牌消息缓存
+	std::vector<const Card*> msgbufCards;//卡牌消息缓存
 	//整局游戏控制相关
 	CardLib& cards;
 	ServerMB msgBox;
 	gameState presentState;
 	std::vector<Player> players;//所有玩家
-	std::vector<Card*> deck;//牌堆
-	std::vector<Card*> droppeddeck;//弃牌堆
+	std::vector<const Card*> deck;//牌堆
+	std::vector<const Card*> droppeddeck;//弃牌堆
 };
 
 #endif
