020f5e9973d83048ae4d8aed1e3ca0d837e357a6
eonian <quyingze@qq.com>
2014-06-10 12:49:54 +0800

    ai继承player版

---
+++ qFluxx/ai/ai.h
@@ -0,0 +1,14 @@
+#include "../cardLib_code/cardLib.h"
+#include "../fluxxControl_code/player.h"
+
+class AI : public Player
+{
+public:
+	AI();
+	void play();
+	void dropCard();
+	void dropKeeper();
+protected:
+
+
+};
